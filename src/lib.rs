4848725d8915a2b8bd5500938e4bdc99d0963863
Nikita Yazikov <cour1337@gmail.com>
2019-09-15 01:43:23 +0300

    added support smbios rev 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod smbios {
-    use std::fs::File;
+    use std::fs::{File, read_dir};
     use std::io::{Read, Seek, SeekFrom};
 
     fn read_raw_mem(address: u64, bytes_count: usize) -> Result<Vec<u8>, u8> {
@@ -19,10 +19,10 @@ pub mod smbios {
     }
 
     const ANCHOR_REV_3_LENGHT: usize = 24;
-    const ANCHOR_REV_2_LENGHT: usize = 31;
+    const ANCHOR_REV_2_LENGHT: usize = 32;
     const SMBIOS_ANCHOR_AREA_START: u64 = 0x000F0000;
     const SMBIOS_ANCHOR_AREA_END: u64 = 0x000FFFFF;
-    const SMBIOS_ANCHOR_UTIL_SIZE: usize = 3;
+    const SMBIOS_ANCHOR_UTIL_SIZE: usize = 4;
 
     enum AnchorVer {
         Rev2,
@@ -42,11 +42,39 @@ pub mod smbios {
 
     impl Anchor {
         pub fn new(address: u64, rev: AnchorVer) -> Anchor {
-            let anchor_raw_data = match rev {
-                AnchorVer::Rev2 => read_raw_mem(address, ANCHOR_REV_2_LENGHT).unwrap(),
-                AnchorVer::Rev3 => read_raw_mem(address, ANCHOR_REV_3_LENGHT).unwrap(),
+
+            let parsed_anchor = match rev {
+                AnchorVer::Rev2 => {
+                    Anchor::create_anchor_rev_2(read_raw_mem(address, ANCHOR_REV_2_LENGHT).unwrap())
+                },
+                AnchorVer::Rev3 => {
+                    Anchor::create_anchor_rev_3(read_raw_mem(address, ANCHOR_REV_3_LENGHT).unwrap())
+                },
             };
 
+            parsed_anchor
+        }
+
+        fn create_anchor_rev_2 (anchor_raw_data: Vec<u8>) -> Anchor {
+            let mut raw_struct_max_size: [u8; 2] = [0; 2];
+            raw_struct_max_size.copy_from_slice(&anchor_raw_data[22..24]);
+
+            let mut raw_table_address: [u8; 4] = [0; 4];
+            raw_table_address.copy_from_slice(&anchor_raw_data[24..28]);
+
+            Anchor {
+                entry_point_checksum: anchor_raw_data[4],
+                entry_point_lenght: anchor_raw_data[5],
+                major_version: anchor_raw_data[6],
+                minor_version: anchor_raw_data[7],
+                doc_rev: 0,
+                entry_point_rev: anchor_raw_data[10],
+                structure_table_max_size: u16::from_le_bytes(raw_struct_max_size) as u32,
+                structure_table_address: u32::from_le_bytes(raw_table_address) as u64,
+            }
+        }
+
+        fn create_anchor_rev_3 (anchor_raw_data: Vec<u8>) -> Anchor {
             let mut raw_struct_max_size: [u8; 4] = [0; 4];
             raw_struct_max_size.copy_from_slice(&anchor_raw_data[12..16]);
 
@@ -64,24 +92,19 @@ pub mod smbios {
                 structure_table_address: u64::from_le_bytes(raw_table_address),
             }
         }
-
-        fn create_anchor_rev_2 (raw_anchor_data: Vec<u8>) -> Anchor {
-
-        }
-
-        fn create_anchor_rev_3 (raw_anchor_data: Vec<u8>) -> Anchor {
-
-        }
     }
 
     fn get_smbios_anchor() -> Result<Anchor, u8> {
-        for address in SMBIOS_ANCHOR_AREA_START..SMBIOS_ANCHOR_AREA_END - 3 {
+        for address in SMBIOS_ANCHOR_AREA_START..SMBIOS_ANCHOR_AREA_END - 4 {
             let data = read_raw_mem(address, SMBIOS_ANCHOR_UTIL_SIZE).unwrap();
             let ascii_data = String::from_utf8(data);
 
             if let Ok(ascii) = ascii_data {
-                if ascii.to_ascii_uppercase() == "_SM".to_ascii_uppercase() {
-                    return Ok(Anchor::new(address));
+                if ascii.to_ascii_uppercase() == "_SM_".to_ascii_uppercase() {
+                    return Ok(Anchor::new(address, AnchorVer::Rev2));
+                }
+                else if ascii.to_ascii_uppercase() == "_SM3".to_ascii_uppercase() {
+                    return Ok(Anchor::new(address, AnchorVer::Rev3));
                 }
             }
         }
@@ -103,7 +126,7 @@ pub mod smbios {
             let anchor = get_smbios_anchor();
             assert!(anchor.is_ok());
             let good_anchor = anchor.unwrap();
-            assert_eq!(good_anchor.entry_point_checksum, 0x01);
+            assert_eq!(good_anchor.doc_rev, 0x00);
         }
     }
 
